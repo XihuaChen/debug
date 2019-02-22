f96205a0b5e3d659ce1a113e19b22727a34c9c07
xihua chen <xihuas.chen@gmail.com>
2019-02-21 17:02:37 -0800

    python scripts

---
+++ main_cor.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+'''
+    A demo of calling functions in another local file.
+    
+'''
+
+import sys
+import myFunctions as mf
+import pandas as pd
+
+if __name__ == '__main__':
+    # read data
+    data = pd.read_csv("data.csv", index_col=0)
+
+    # overview of data
+    print("dimensions: {}".format(data.shape))
+
+    # calculate correlation for nonzero values
+    x = data.ix[:,1]
+    y = data.ix[:,2]
+    result = mf.nz_corr(x, y)
+    print("nonzero corr: {}".format(result))
+
+    x = data.ix[:,2]
+    y = data.ix[:,3]
+    result = mf.nz_corr(x,y)
+    print("nonzero corr: {}".format(result))
